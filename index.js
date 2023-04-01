3fcf2309f036f23ca95c9a26d3611966a37f9f59
Julian Victorio <julianv714@gmail.com>
2023-03-31 18:14:10 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    const driverData = drivers.filter(checkMatchingDriver)
+
+    function checkMatchingDriver(driver) {
+        return driver === string || driver === string.toLowerCase()
+    }
+    return driverData
+}
+
+function fuzzyMatch(drivers, string) {
+    const driverData = drivers.filter(checkMatchingLetter)
+
+    function checkMatchingLetter(letters) {
+        return letters.startsWith(string)
+    }
+    return driverData
+}
+
+function matchName (drivers, string) {
+    const driverData = drivers.filter(checkMatchingName) 
+
+    function checkMatchingName (driver) {
+        return driver.name === string
+    }
+    return driverData
+}
